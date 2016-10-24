1d8a6ef88354c577f2d8bd2a6c2ecde68e92f065
rajkumbhar <rajkumb.rk@gmail.com>
2016-10-24 14:45:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.get('/ui/logo.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
 });
 
+app.get('/ui/logo/adidas.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'logo/adidas.png'));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
